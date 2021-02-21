144ba24fc039561462d79bfe9b5f88ee708c0ef1
Gabriela Vespoli <gabiat3@hotmail.com>
2021-02-21 18:33:52 -0300

    up

--- src/controller/AnunciosController.js
+++ src/controller/AnunciosController.js
@@ -1,31 +1,55 @@
-const { validationResult } = require("express-validator");
+const { validationResult } = require('express-validator');
 
-const anuncio = require("../model/Anuncios");
+const anuncio = require('../model/Anuncios');
 
 class AnunciosController {
-  getAllAction(req, res) {
-    anuncio.getAll(req, res);
-  }
+    getAllAction(req,res) {
+        anuncio.getAll(req,res)
+    }
 
-  postarAnuncioAction(req, res) {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+    postarAnuncioAction(req,res) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { id_vendedor, descricao, valor, doacao } = req.body
+        const img = req.file
+
+        if(!img) {
+            return res.status(400).json({errors: [{msg: "* campo obrigatório!",param: "img"}]})
+        }
+        
+        anuncio.id_vendedor = id_vendedor;
+        anuncio.img = img.filename;
+        anuncio.descricao = descricao;
+        anuncio.valor = valor;
+        anuncio.doacao = doacao;
+        anuncio.postarAnuncio(req,res)
     }
-    const { id, descricao, valor, doacao } = req.body;
-    const img = req.file;
-    if (!img) {
-      return res
-        .status(400)
-        .json({ errors: [{ msg: "* campo obrigatório!", param: "img" }] });
+
+    editarAnuncioAction(req,res) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { id_vendedor, descricao, valor, doacao } = req.body
+        const img = req.file
+        const { id } = req.params
+
+        if(!img) {
+            return res.status(400).json({errors: [{msg: "* campo obrigatório!",param: "img"}]})
+        }
+
+        anuncio.id = id;
+        anuncio.id_vendedor = id_vendedor;
+        anuncio.img = img.filename;
+        anuncio.descricao = descricao;
+        anuncio.valor = valor;
+        anuncio.doacao = doacao;
+        anuncio.editarAnuncio(req,res)
     }
-    anuncio.id_vendedor = id;
-    anuncio.img = img.filename;
-    anuncio.descricao = descricao;
-    anuncio.valor = valor;
-    anuncio.doacao = doacao;
-    anuncio.postarAnuncio(req, res);
-  }
 }
 
 module.exports = new AnunciosController
