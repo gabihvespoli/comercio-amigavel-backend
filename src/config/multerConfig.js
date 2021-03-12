ee82a12b2077dc6d0faeccdf461fa3b20ca60bf9
Gabriela Vespoli <gabiat3@hotmail.com>
2021-03-12 16:31:44 -0300

    add aws s3

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -1,13 +1,31 @@
 const multer = require('multer');
+const multerS3 = require('multer-s3');
+const aws = require('aws-sdk');
 const crypto = require('crypto');
 
-const multerAnunciosConfig =  {
-    dest: 'uploads/',
-    storage: multer.diskStorage({
+const storageTypes = {
+    local: multer.diskStorage({
         destination: (req,file,cb) => {
-            cb(null,'uploads/');            
+
+            cb(null,'uploads/');
+
         },
         filename: (req,file,cb) => {
+            crypto.randomBytes(16, (err, hash) => {
+                if (err) cb(err)
+
+                file.key =`${hash.toString('hex') + file.mimetype.replace('image/','.')}`
+
+                cb(null,file.key)
+            })
+        },
+    }),
+    s3: multerS3({
+        s3: new aws.S3(),
+        bucket: 'comercioamigavel',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        acl: 'public-read',
+        key: (req,file,cb) => {
             crypto.randomBytes(16, (err, hash) => {
                 if (err) cb(err)
 
@@ -17,6 +35,11 @@ const multerAnunciosConfig =  {
             })
         },
     }),
+}
+
+const multerAnunciosConfig =  {
+    dest: 'uploads/',
+    storage: storageTypes[process.env.STORAGE_TYPE],
     limits: {
         fileSize: 5 * 1024 * 1024,
     },
