1f521cae904083971e59ac7f17f82221dc3c6510
Gabriela Vespoli <gabiat3@hotmail.com>
2021-03-22 22:26:41 -0300

    test cors-anywhere

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+// const app = require('./src/app');
+
+// const PORT = process.env.PORT || 3333
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`)
+// });
+
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 3333
+var HOST = process.env.HOST || '0.0.0.0';
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3333;
+
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(PORT, HOST, function() {
+    console.log('Running CORS Anywhere on ' + HOST + ':' + PORT);
 });
