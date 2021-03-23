71502e7c77a6b97600da55bf155b7bc06040683e
Gabriela Vespoli <gabiat3@hotmail.com>
2021-03-22 23:02:05 -0300

    up

--- src/app.js
+++ src/app.js
@@ -14,8 +14,8 @@ class App {
 
     middlewares() {
         this.server.use((req, res, next) => {
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+            res.header("Access-Control-Allow-Origin", "https://comercio-amigavel-frontend.herokuapp.com");
+            // res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
             // res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
             this.server.use(cors());
             next();
