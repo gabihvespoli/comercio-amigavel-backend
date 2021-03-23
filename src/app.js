50cf7b01a87d75888fff7090e19ca542394f1ca5
Gabriela Vespoli <gabiat3@hotmail.com>
2021-03-23 00:04:29 -0300

    up

--- src/app.js
+++ src/app.js
@@ -12,14 +12,20 @@ class App {
         this.routes()
     }
 
+    // middlewares() {
+    //     this.server.use((req, res, next) => {
+    //         res.header("Access-Control-Allow-Origin", "*");
+    //         // res.header("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Authorization");
+    //         res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    //         this.server.use(cors());
+    //         next();
+    //     });
+    //     this.server.use(express.json());
+    //     this.server.use('/uploads',express.static('uploads'))
+    // }
+    
     middlewares() {
-        this.server.use((req, res, next) => {
-            res.header("Access-Control-Allow-Origin", "*");
-            // res.header("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Authorization");
-            res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-            this.server.use(cors());
-            next();
-        });
+        this.server.use(cors());
         this.server.use(express.json());
         this.server.use('/uploads',express.static('uploads'))
     }
