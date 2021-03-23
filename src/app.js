d80cad9bd9dbd787f46aceacbbf34946f404daa7
Gabriela Vespoli <gabiat3@hotmail.com>
2021-03-22 23:08:34 -0300

    up

--- src/app.js
+++ src/app.js
@@ -14,10 +14,17 @@ class App {
 
     middlewares() {
         this.server.use((req, res, next) => {
-            res.header("Access-Control-Allow-Origin", "https://comercio-amigavel-frontend.herokuapp.com");
+            res.header("Access-Control-Allow-Origin");
+            res.header(
+                "Access-Control-Allow-Headers",
+                "Origin, X-Requested-With, Content-Type, Accept"
+            );
             // res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
             // res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-            this.server.use(cors());
+            this.server.use(cors({
+                origin: "https://comercio-amigavel-frontend.herokuapp.com/",
+                methods: "GET,PUT,POST,DELETE"
+            }));
             next();
         });
         this.server.use(express.json());
